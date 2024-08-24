13f79503151f08e3441aa1fc4c9e45aa59609afc
lashagiorgiazaladze <daxmarebaminda@gmail.com>
2024-08-24 10:52:08 -0700

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('myBtn').onclick = function() {
     window.open('http://127.0.0.1:5500/learnMore.html');
 };
+const myDiv = document.getElementById("myDiv")
+myDiv.onclick = function() {
+    window.open('http://127.0.0.1:5500/aboutme.html');
+}
+const MyDiv = document.getElementById("MyDiv")
+MyDiv.onclick = function() {
+    window.open('');
+}
